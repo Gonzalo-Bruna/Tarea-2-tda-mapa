fd920f1636d87958738a5576e9c129e3ca4d73ba
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2020-05-10 14:24:12 -0400

    añadido el gitignore y el main.c mostrando opciones principales en pantalla

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+
+    while(1){
+
+
+        printf("¡Bienvenido a nuestro sistema de organizacion de productos!");
+        printf("\n");
+        printf("Por favor, elija una de las siguientes opciones: ");
+        printf("\n");
+        printf("\n");
+        printf("opcion 1: Importar productos desde un archivo");
+        printf("\n");
+        printf("opcion 2: Exportar productosa a un archivo csv");
+        printf("\n");
+        printf("opcion 3: Agregar un producto");
+        printf("\n");
+        printf("opcion 4: Buscar productos por tipo");
+        printf("\n");
+        printf("opcion 5: Buscar productos por marca");
+        printf("\n");
+        printf("opcion 6: Buscar producto por nombre");
+        printf("\n");
+        printf("opcion 7: Mostrar todos los productos");
+        printf("\n");
+        printf("opcion 8: Agregar un producto al carrito");
+        printf("\n");
+        printf("opcion 9: Concretar compra");
+        printf("\n");
+        printf("\n");
+
+
+        size_t opcion;
+
+        break;
+
+    }
+
+    system("pause");
+
+    return 0;
+}
