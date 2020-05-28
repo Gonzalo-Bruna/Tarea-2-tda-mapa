06a24f22934af7e2f458fb6d4403266ec87ab392
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2020-05-28 13:49:20 -0400

    añadidos los structs producto y carrito

--- functions.h
+++ functions.h
@@ -1,6 +1,20 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+typedef struct {
 
+    char * nombre;
+    char * marca;
+    char * tipo;
+    int stock;
+    int precio;
+
+} producto;
+
+typedef struct{
+
+    List * productos;
+
+} carrito;
 
 #endif
