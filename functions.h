c30534c1535051f3f5aff99f588f3028e885ba17
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2020-05-28 13:52:48 -0400

    añadidas las funciones para trabajar archivos csv

--- functions.h
+++ functions.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+//struct producto
 typedef struct {
 
     char * nombre;
@@ -11,10 +12,19 @@ typedef struct {
 
 } producto;
 
+//struct carrito
 typedef struct{
 
     List * productos;
 
 } carrito;
+
+//funcion que duplica un string
+char * _strdup (const char *s);
+
+//funcion que obtiene un campo de un archivo csv
+const char *get_csv_field (char * tmp, int i);
+
+
 
 #endif
