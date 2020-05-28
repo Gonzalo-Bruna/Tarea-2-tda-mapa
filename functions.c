e79bd895d511e58b304d7f8ac866bd65f8ba97d6
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2020-05-28 13:59:38 -0400

    arreglada la inclusión de los structs

--- functions.c
+++ functions.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+#include "functions.h"
+#include "list.h"
+
+struct producto{
+
+    char * nombre;
+    char * marca;
+    char * tipo;
+    int stock;
+    int precio;
+
+};
+
+struct carrito{
+
+    List * productos;
+
+};
+
 char * _strdup (const char *s) {
     size_t len = strlen (s) + 1;
     void *new = malloc (len);
--- functions.h
+++ functions.h
@@ -2,29 +2,15 @@
 #define FUNCTIONS_H
 
 //struct producto
-typedef struct {
-
-    char * nombre;
-    char * marca;
-    char * tipo;
-    int stock;
-    int precio;
-
-} producto;
+typedef struct producto producto;
 
 //struct carrito
-typedef struct{
-
-    List * productos;
-
-} carrito;
+typedef struct carrito carrito;
 
 //funcion que duplica un string
 char * _strdup (const char *s);
 
 //funcion que obtiene un campo de un archivo csv
 const char *get_csv_field (char * tmp, int i);
-
-
 
 #endif
